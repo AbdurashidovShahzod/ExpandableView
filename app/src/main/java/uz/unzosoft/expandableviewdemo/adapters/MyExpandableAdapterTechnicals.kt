57006239f65800afe544e7796a8fcab4a6eb028a
AbdurashidovShahzod <shahzod9933@gmail.com>
2020-12-27 01:32:28 +0500

    data

---
+++ app/src/main/java/uz/unzosoft/expandableviewdemo/adapters/MyExpandableAdapterTechnicals.kt
@@ -0,0 +1,89 @@
+package uz.unzosoft.expandableviewdemo.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import android.widget.ListAdapter
+import kotlinx.android.synthetic.main.child_item.view.*
+import kotlinx.android.synthetic.main.group_item.view.*
+import kotlinx.android.synthetic.main.group_item_technical.view.*
+import uz.unzosoft.expandableviewdemo.R
+
+class MyExpandableAdapterTechnicals(
+    var listTitle: List<String>,
+    var map: HashMap<String, List<String>>
+) :
+    BaseExpandableListAdapter() {
+
+
+    override fun getGroupCount(): Int {
+        return listTitle.size
+    }
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return map[listTitle[groupPosition]]!!.size
+    }
+
+    override fun getGroup(groupPosition: Int): String {
+        return listTitle[groupPosition]
+    }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        val list = map[listTitle[groupPosition]]
+        return list!![childPosition]
+    }
+
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getGroupView(
+        groupPosition: Int,
+        childPosition: Boolean,
+        p2: View?,
+        p3: ViewGroup?
+    ): View {
+        val groupItemView: View
+        if (p2 == null) {
+            groupItemView =
+                LayoutInflater.from(p3?.context).inflate(R.layout.group_item_technical, p3, false)
+        } else {
+            groupItemView = p2
+        }
+        groupItemView.tvGroupTechnical.text = listTitle[groupPosition]
+        return groupItemView
+    }
+
+    override fun getChildView(
+        groupPosition: Int,
+        childPosition: Int,
+        p2: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        var childItemView: View
+        if (convertView == null) {
+            childItemView = LayoutInflater.from(parent?.context)
+                .inflate(R.layout.child_item_technical, parent, false)
+        } else {
+            childItemView = convertView
+        }
+        val list = map[listTitle[groupPosition]]
+        val childPosition = list!![childPosition]
+        childItemView.tvChild.text = childPosition
+        return childItemView
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+}
--- app/src/main/java/uz/unzosoft/expandableviewdemo/ui/SecondActivity.kt
+++ app/src/main/java/uz/unzosoft/expandableviewdemo/ui/SecondActivity.kt
@@ -3,10 +3,19 @@ package uz.unzosoft.expandableviewdemo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import uz.unzosoft.expandableviewdemo.R
+import uz.unzosoft.expandableviewdemo.adapters.MyExpandableAdapter
+import uz.unzosoft.expandableviewdemo.adapters.MyExpandableAdapterTechnicals
 
 class SecondActivity : AppCompatActivity() {
+    private var listTitle = ArrayList<String>()
+    private lateinit var map: HashMap<String, List<String>>
+    private lateinit var adapter: MyExpandableAdapterTechnicals
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+
     }
 }
