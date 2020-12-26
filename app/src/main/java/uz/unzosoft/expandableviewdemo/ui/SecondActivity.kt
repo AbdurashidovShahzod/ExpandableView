cbc51cd28852cfbd3a8a7fda0689568f84c6cf5c
AbdurashidovShahzod <shahzod9933@gmail.com>
2020-12-27 01:55:46 +0500

    data

--- app/src/main/java/uz/unzosoft/expandableviewdemo/ui/SecondActivity.kt
+++ app/src/main/java/uz/unzosoft/expandableviewdemo/ui/SecondActivity.kt
@@ -2,6 +2,7 @@ package uz.unzosoft.expandableviewdemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 import uz.unzosoft.expandableviewdemo.R
 import uz.unzosoft.expandableviewdemo.adapters.MyExpandableAdapter
 import uz.unzosoft.expandableviewdemo.adapters.MyExpandableAdapterTechnicals
@@ -15,7 +16,36 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        initData()
+        adapter = MyExpandableAdapterTechnicals(listTitle, map)
+        expandableTechnicalListView.setAdapter(adapter)
+
+    }
 
 
+    private fun initData() {
+        listTitle.add("Mobiles")
+        listTitle.add("Laptops")
+        listTitle.add("Computer Accessories")
+        listTitle.add("Home Entertainment")
+        listTitle.add("TVs by brand")
+        listTitle.add("Kitchen appliances")
+        val mobilesList =
+            arrayListOf("Mi", "Honor", "Realme", "Samsung", "Infinix", "Oppo", "Apple")
+        val laptopsList = arrayListOf("Mac", "Dell", "Acer")
+        val computerAccessoryList = arrayListOf("Pendrive", "Bag", "Mouse")
+        val homeEntertainmentList =
+            arrayListOf("Home audion speaker", "Home theatres", "Bluetooth speakers")
+        val tvList = arrayListOf("Lg", "Samsung")
+        val kitchenList = arrayListOf("Smart fryer", "Microwave")
+
+
+        map = HashMap()
+        map["Mobiles"] = mobilesList
+        map["Laptops"] = laptopsList
+        map["Computer Accessories"] = computerAccessoryList
+        map["Home Entertainment"] = homeEntertainmentList
+        map["TVs by brand"] = tvList
+        map["Kitchen appliances"] = kitchenList
     }
 }
